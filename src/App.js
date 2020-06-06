2ffa7b3ea477d16e0d7b0a71517349875093bc52
Jordan Lopez <jordanlopez992@gmail.com>
2020-06-06 09:32:19 -0400

    Previous work

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import AddBookmark from './addBookmark/addBookmark';
+import BookmarkApp from './bookmarkApp/bookmarkApp';
+
+
 class App extends Component {
+
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      bookmarks: [],
+      showAddForm: false
+    };
+  }
+
+
   render() {
+
+
+    const page = this.state.showAddForm
+          ? <AddBookmark />
+          : <BookmarkApp bookmarks={this.state.bookmarks}/>;
+
+
     return (
       <div className="App">
+        { page }
       </div>
     );
+
+
   }
+
+
 }
 
 export default App;
